a090c9cada0d8bdadb69f5ac6bef0a5b9eb9ebad
@ <oe@qodir.local>
2026-03-26 22:21:58 +0500

    written test

--- weather_site/weather/tests.py
+++ weather_site/weather/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class WeatherTestCase(TestCase):
+
+    def test_weather_endpoint(self):
+        response = self.client.get('/weather/Tashkent/')
+        self.assertEqual(response.status_code, 200)
